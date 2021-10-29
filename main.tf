7a1c2972bd4873805fa49987ca98d43f5d382aa5
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-10-29 15:21:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,10 @@ provider "google" {
 }
 
 
-resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
-  name                  = "l7-ilb-forwarding-rule"
-  provider              = google-beta
-  region                = "europe-west1"
-  depends_on            = [google_compute_subnetwork.proxy_subnet]
+resource "google_compute_forwarding_rule" "example" {
+  name                  = "${local.resource_prefix}-<name>-tcpilb"
   ip_protocol           = "TCP"
   load_balancing_scheme = "INTERNAL_MANAGED"
-  port_range            = "80"
-  target                = google_compute_region_target_http_proxy.default.id
-  network               = google_compute_network.ilb_network.id
-  subnetwork            = google_compute_subnetwork.ilb_subnet.id
-  network_tier          = "PREMIUM"
+  port_range            = "443"
+  ...
 }
